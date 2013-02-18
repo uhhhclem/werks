da82c8d8e4adce71f42ae74c6bb540dd3fef937f
Robert Rossney <rbr@well.com>
2013-02-18 15:47:42 -0800

    Eliminate module-level vars from users.

--- src/user/user.go
+++ src/user/user.go
@@ -16,35 +16,42 @@ import (
 var InvalidNicknameError = errors.New("Invalid nickname.")
 var InvalidPasswordError = errors.New("Invalid password.")
 
-var passwordSalt string
-var userFilename string
-var users []*User
-var usersByNickname map[string]*User
-var usersById map[string]*User
+// Users is a persistable collection of users.
+type Users struct {
+  passwordSalt string
+  userFilename string
+  users []*User
+  usersByNickname map[string]*User
+  usersById map[string]*User
+}
 
+// User is an individual user.
 type User struct {
 	Id string 	`json:"id"`
 	Nickname string `json:"nickname"`
 	Pwhash string `json:"pwhash"`
 }
 
-// Init initializes the user module.
-func Init(salt string, filename string) {
-	userFilename = filename
-	passwordSalt = salt
+// Init creates a new Users structure.
+func Init(salt string, filename string) *Users {
+	u := new(Users)
+	u.userFilename = filename
+	u.passwordSalt = salt
 
-	users = make([]*User, 0)
-	usersByNickname = make(map[string]*User)
-	usersById = make(map[string]*User)
+	u.users = make([]*User, 0)
+	u.usersByNickname = make(map[string]*User)
+	u.usersById = make(map[string]*User)
+
+	return u
 }
 
 // New creates a new user, with the given nickname and password.  It returns
 // the user created, or an error if the nickname already exists.  The nickname
 // is canonicalized, and the password is salted and hashed.
-func New(nickname string, password string) (*User, error) {
+func (s *Users) New(nickname string, password string) (*User, error) {
 
-	if passwordSalt == "" {
-		return nil, errors.New("Must call Init before creating any user.")
+	if s.passwordSalt == "" {
+		return nil, errors.New("No password salt found.")
 	}
 
 	id, err := uuid.GenUUID()
@@ -55,40 +62,40 @@ func New(nickname string, password string) (*User, error) {
 	u := &User {
 		Id: id,
 		Nickname: nickname,
-		Pwhash: hashPassword(canonicalizePassword(password))}
+		Pwhash: s.hashPassword(canonicalizePassword(password))}
 
 	key := canonicalizeNickname(u.Nickname)
-	if usersByNickname[key] != nil {
+	if s.usersByNickname[key] != nil {
 		return nil, errors.New("Duplicate nickname.")
 	}
 
-	users = append(users, u)
-	usersByNickname[key] = u
-	usersById[u.Id] = u
+	s.users = append(s.users, u)
+	s.usersByNickname[key] = u
+	s.usersById[u.Id] = u
 
 	return u, nil
 }
 
 // Login validates a user login and returns the User.
-func Login(nickname, password string) (*User, error) {
-	u := LookupByNickname(nickname)
+func (s *Users) Login(nickname, password string) (*User, error) {
+	u := s.LookupByNickname(nickname)
 	if u == nil {
 		return nil, InvalidNicknameError
 	}
-	if u.Pwhash != hashPassword(canonicalizePassword(password)) {
+	if u.Pwhash != s.hashPassword(canonicalizePassword(password)) {
 		return nil, InvalidPasswordError
 	}
 	return u, nil
 }
 
 // LookupById returns the User with the given ID, or nil if none exists.
-func LookupById(id string) *User {
-	return usersById[id]
+func (s *Users) LookupById(id string) *User {
+	return s.usersById[id]
 }
 
 // LookupByNickname returns the User with the given nickname, or nil if none exists.
-func LookupByNickname(nickname string) *User {
-	return usersByNickname[canonicalizeNickname(nickname)]
+func (s *Users) LookupByNickname(nickname string) *User {
+	return s.usersByNickname[canonicalizeNickname(nickname)]
 }
 
 // canonicalizeNickname strips non-alpha-numeric characters from
@@ -107,32 +114,32 @@ func canonicalizePassword(password string) string {
 }
 
 // hashPassword salts and hashes a password.
-func hashPassword(password string) string {
+func (s *Users) hashPassword(password string) string {
 
 	h := sha1.New()
-	io.WriteString(h, passwordSalt)
+	io.WriteString(h, s.passwordSalt)
 	io.WriteString(h, password)
 
 	return fmt.Sprintf("%x", h.Sum(nil))
 }
 
 // LoadUsers loads users saved in userFilename.
-func LoadUsers() error {
-	if userFilename == "" {
-		return errors.New("Must call Init to set user filename first.")
+func (s *Users) LoadUsers() error {
+	if s.userFilename == "" {
+		return errors.New("Must set user filename first.")
 	}
 	var err error
 	var f *os.File
 	var fi os.FileInfo
 	var b []byte
 
-	fi, err = os.Stat(userFilename)
+	fi, err = os.Stat(s.userFilename)
 	if err != nil {
 		return err
 	}
 
 	b = make([]byte, fi.Size())
-	f, err = os.Open(userFilename)
+	f, err = os.Open(s.userFilename)
 	if err != nil {
 		return err
 	}
@@ -143,16 +150,16 @@ func LoadUsers() error {
 		return err
 	}
 
-	err = json.Unmarshal(b, &users)
+	err = json.Unmarshal(b, &s.users)
 	if err != nil {
 		return err
 	}
 
-	usersByNickname = make(map[string] *User)
-	usersById = make(map[string] *User)
-	for _, u := range users {
-		usersByNickname[canonicalizeNickname(u.Nickname)] = u
-		usersById[u.Id] = u
+	s.usersByNickname = make(map[string] *User)
+	s.usersById = make(map[string] *User)
+	for _, u := range s.users {
+		s.usersByNickname[canonicalizeNickname(u.Nickname)] = u
+		s.usersById[u.Id] = u
 	}
 
 	return nil
@@ -160,22 +167,22 @@ func LoadUsers() error {
 }
 
 // SaveUsers writes the users out to userFilename.
-func SaveUsers() error {
-	if userFilename == "" {
-		return errors.New("Must call Init to set user filename first.")
+func (s *Users) SaveUsers() error {
+	if s.userFilename == "" {
+		return errors.New("Must set user filename first.")
 	}
-	if users == nil {
+	if s.users == nil {
 		return errors.New("Must load or initialize users first.")
 	}
 	var b []byte
 	var f *os.File
 	var err error
 
-	b, err = json.Marshal(users)
+	b, err = json.Marshal(s.users)
 	if err != nil {
 		return err
 	}
-	f, err = os.Create(userFilename)
+	f, err = os.Create(s.userFilename)
 	if err != nil {
 		return err
 	}
--- src/user/user_test.go
+++ src/user/user_test.go
@@ -11,21 +11,21 @@ func TestNew(t *testing.T) {
 	var u *User
 	var err error
 
-	Init(salt, filename)
+	s := Init(salt, filename)
 
-	u, err = New("nickname", "password")
+	u, err = s.New("nickname", "password")
 	if err != nil {
 		t.Errorf("%s", err)
 		return
 	}
-	if LookupByNickname("Nickname") != u {
+	if s.LookupByNickname("Nickname") != u {
 		t.Errorf("LookupByNickname failed.")
 	}
-	if LookupById(u.Id) != u {
+	if s.LookupById(u.Id) != u {
 		t.Errorf("LookupById failed.")
 	}
 
-	u, err = New("nickname", "password")
+	u, err = s.New("nickname", "password")
 	if err == nil {
 		t.Errorf("Created user with a duplicate nickname.")
 	}
@@ -60,16 +60,16 @@ func TestLogin(t *testing.T) {
 	var nicknames = []string {"Alpha", "Beta", "Gamma", "Delta"}
 	var passwords = []string {"Epsilon", "Omicron", "Omega", "Upsilon"}
 
-	Init(salt, filename)
+	s := Init(salt, filename)
 
 	for i, _ := range nicknames {
-		New(nicknames[i], passwords[i])
+		s.New(nicknames[i], passwords[i])
 	}
 
 	var user *User
 	var err error
 
-	user, err = Login("..Alpha", "Epsilon")
+	user, err = s.Login("..Alpha", "Epsilon")
 	if user == nil {
 		if err == nil {
 			t.Errorf("Should have gotten an error.")
@@ -79,7 +79,7 @@ func TestLogin(t *testing.T) {
 		return
 	}
 
-	user, err = Login("  alpha  ", "Slipshod")
+	user, err = s.Login("  alpha  ", "Slipshod")
 	if user != nil {
 		t.Errorf("Login should have failed.");
 		return
@@ -89,7 +89,7 @@ func TestLogin(t *testing.T) {
 		return
 	}
 
-	user, err = Login("Bogus", "Slipshod")
+	user, err = s.Login("Bogus", "Slipshod")
 	if user != nil {
 		t.Errorf("Login should have failed.");
 		return
@@ -105,38 +105,38 @@ func TestSaveAndLoad(t *testing.T) {
 	var nicknames = []string {"Alpha", "Beta", "Gamma", "Delta"}
 	var passwords = []string {"Epsilon", "Omicron", "Omega", "Upsilon"}
 
-	Init(salt, filename)
+	s := Init(salt, filename)
 
 	for i, _ := range nicknames {
-		New(nicknames[i], passwords[i])
+		s.New(nicknames[i], passwords[i])
 	}
 	for i, _ := range nicknames {
-		if LookupByNickname(nicknames[i]) == nil {
+		if s.LookupByNickname(nicknames[i]) == nil {
 			t.Errorf("LookupByNickname failed.")
 			return
 		}
 	}
-	err := SaveUsers()
+	err := s.SaveUsers()
 	if err != nil {
 		t.Errorf("%s", err)
 	}
 
-	Init(salt, filename)
+	s = Init(salt, filename)
 
 	for i, _ := range nicknames {
-		if LookupByNickname(nicknames[i]) != nil {
+		if s.LookupByNickname(nicknames[i]) != nil {
 			t.Errorf("LookupByNickname shouldn't have found anything.")
 			return
 		}
 	}
 
-	err = LoadUsers()
+	err = s.LoadUsers()
 	if err != nil {
 		t.Errorf("%s", err)
 	}
 
 	for i, _ := range nicknames {
-		if LookupByNickname(nicknames[i]) == nil {
+		if s.LookupByNickname(nicknames[i]) == nil {
 			t.Errorf("LookupByNickname failed.")
 			return
 		}
